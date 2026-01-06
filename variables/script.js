7a03cb54ac69dedbdb77d90399dfcfc8a2ddd1a8
Rivika-Ishaira <rivikaishaira@gmail.com>
2026-01-06 16:19:12 +0530

    Declaration + Initiallization

---
+++ variables/script.js
@@ -0,0 +1,20 @@
+// VARIABLES: It is a storage box to store the data. Storage box's name is variable's name.
+
+// Declaration
+var a;
+let b;
+// But we can't only declare const, we have to assign the value(initialize) while declaring it.
+
+// Declaration + Initialization
+var a1 = 10;
+let b1 = 20;
+const pi = 3.14;
+// Here is a catch, we can't reclare let and const.
+// We can't update values of const.
+
+// Demerits of var:
+// 1. It add in window.
+// 2. It is Function scoped.  -> Doesn't obey scope.
+// 3. Redeclaration is possible.
+
+
