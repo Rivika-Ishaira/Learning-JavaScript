07f2496adaf3700c54c2fa146829dc6317763022
Rivika-Ishaira <rivikaishaira@gmail.com>
2026-01-06 16:20:07 +0530

    Scope

--- variables/script.js
+++ variables/script.js
@@ -17,4 +17,48 @@ const pi = 3.14;
 // 2. It is Function scoped.  -> Doesn't obey scope.
 // 3. Redeclaration is possible.
 
+// Scope: A range of an variable where we can use it.
+// Types of scope:
 
+// 1. Function scoped: Accessable inside the function.
+function one() {
+  var a = 10;
+  if (true) {
+    var b = 20;
+    console.log(a);
+    console.log(b);
+  }
+  console.log(b); // Accessing b, but b is inside a block scoped, It is possible because var is function scoped so the variables are accessible in entire function.
+}
+one();
+
+// 2. Block scoped
+function two() {
+  let a = 10;
+  if (true) {
+    let b = 20;
+    const c = 30;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+  }
+  console.log(a);
+  // console.log(b); // cannot access -> Due to block scoped
+  // console.log(c); // cannot access -> Due to block scoped
+}
+two();
+
+// 3. Global scoped
+let globalVariable = "Data"; // Accessible ins whole code.
+function three() {
+  let a = 10;
+  console.log(a);
+  console.log(globalVariable);
+  if (true) {
+    let b = 20;
+    console.log(b);
+    console.log(globalVariable);
+  }
+}
+three();
+console.log(globalVariable);
